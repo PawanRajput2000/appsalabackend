41865f51ed91d597062df9ac2c5420061fdf4e77
Pawan <pawanrajput852710@gmail.com>
2023-08-06 17:59:35 +0530

    final

--- src/controller/appschemaData.js
+++ src/controller/appschemaData.js
@@ -1,20 +1,70 @@
 const appSchema = require("../models/appschema")
+const { uploadFile } = require("../AWS/aws")
 
 const saveProduct = async (req, res) => {
     try {
         let formData = req.body
-        console.log(formData.name)
-        let checkUnique = await appSchema.findOne({name:formData.name})
-        console.log(checkUnique)
-        if(checkUnique){
-            return res.status(400).json({ status: true, data: "name should be unique" }) 
+        console.log(formData)
+        let files = req.files;
+
+        console.log(files);
+
+
+        console.log(formData.logo)
+
+
+        let checkUnique = await appSchema.findOne({ name: formData.slug })
+
+        if (checkUnique) {
+            return res.status(400).json({ status: true, data: "slug should be unique" })
         }
-        
-        let data = await appSchema.create(formData)
-        return res.status(201).json({ status: true, data: data })
+
+        if (files.length === 0 || files[0].fieldname !== "logo") {
+            return res
+                .status(400)
+                .send({
+                    status: false,
+                    message: "required productImage as key and file as value",
+                });
+        }
+
+        if (
+            !(
+                files[0].mimetype == "image/png" ||
+                files[0].mimetype == "image/jpg" ||
+                files[0].mimetype == "image/jpeg"
+            )
+        ) {
+            return res.status(400).send({
+                status: false,
+                message: "Only .png, .jpg and .jpeg format allowed!",
+            });
+        }
+
+        //uploading productImage file in AWS
+        let uploadedFileURL = await uploadFile(files[0]);
+        formData.logo = uploadedFileURL;
+
+        //convert seller details section into object 
+        let updateSeller = formData.sellerDetails
+        formData.sellerDetails = JSON.parse(updateSeller)
+
+
+
+        //convert appPricing Details
+        let UpdateAppPricing = formData.appPricing
+        formData.appPricing = JSON.parse(UpdateAppPricing)
+
+        //convert appMedia Details
+        let UpdateappMedia = formData.appMedia
+        formData.appMedia = JSON.parse(UpdateappMedia)
+
+
+        let saveData = await appSchema.create(formData)
+        return res.status(201).json({ status: true, data: saveData })
 
     } catch (err) {
-        console.group(err.message)
+        console.log("500", err.message)
         return res.status(500).json({ status: true, data: err.message })
     }
 }
@@ -22,8 +72,8 @@ const saveProduct = async (req, res) => {
 const getProduct = async (req, res) => {
     try {
         let data = await appSchema.find();
-        if(!data){
-            return res.status(500).send({ status: false, data: "No Data Found"})
+        if (!data) {
+            return res.status(500).send({ status: false, data: "No Data Found" })
         }
 
 
@@ -52,8 +102,8 @@ const productDetails = async (req, res) => {
     try {
         let slug = req.params.slug
         let data = await appSchema.findOne({ slug: slug })
-        if(!data){
-            return res.status(500).send({ status: false, data: "No Data Found"})
+        if (!data) {
+            return res.status(500).send({ status: false, data: "No Data Found" })
         }
         return res.status(200).send({ status: true, data: data })
     } catch (err) {
@@ -64,4 +114,4 @@ const productDetails = async (req, res) => {
 
 
 
-module.exports = { getProduct, saveProduct, productDetails}
+module.exports = { getProduct, saveProduct, productDetails }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const router = require("./routes/routes")
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const router = require("./routes/routes");
 const cors = require("cors")
-const multer = require ('multer');
+const app = express();
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json());
+app.use(multer().any());
+app.use(express.urlencoded({ extended: true }));
 
-app.use (multer().any())
-mongoose.connect("mongodb+srv://indiaProject:LEsbp5k9osyQcp6Q@indiaproject.1go0lry.mongodb.net/", { useNewUrlParser: true }).then(() => {
-    console.log("connected to DB ")
-}).catch(err => console.log(err.message))
+mongoose.connect(
+    "mongodb+srv://indiaProject:LEsbp5k9osyQcp6Q@indiaproject.1go0lry.mongodb.net/",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err.message));
 
+app.use("/", router);
 
-app.use("/", router)
+app.use((req, res) =>
+  res.status(404).send({ status: false, message: "invalid Path url" })
+);
 
-app.listen(5000, () => {
-    console.log("server started on port 5000")  
-})   
+app.listen(5000, function () {
+  console.log("Express app running on port " + 5000);
+});
