60cbf088a0c96a6edfa8d7c848a3c4c48a078ed3
Pawan <pawanrajput852710@gmail.com>
2023-08-21 16:33:27 +0530

    done

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,58 +1,54 @@
-const jwt = require("jsonwebtoken")
-const mongoose = require("mongoose")
-const userSchema = require("../src/models/userModel")
-const ObjectId = mongoose.Types.ObjectId
-
-
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const userSchema = require("../src/models/userModel");
+const ObjectId = mongoose.Types.ObjectId;
 
 const authentication = async (req, res, next) => {
     try {
-        const token = req.headers["x-api-token"];
-       
-        if (!token) {
+        const authHeader = req.headers.authorization;
+        
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
             return res.status(401).json({ status: false, data: "Token must be provided" });
         }
+
+        const token = authHeader.split(" ")[1];
 
         jwt.verify(token, "osnilWebSolution", function (err, decoded) {
             if (err) {
                 return res.status(401).send({ status: false, message: err.message });
             }          
             req.decoded = decoded;
-           next();
-        });  
+            next();
+        });
     } catch (err) {
         return res.status(500).json({ status: false, message: err.message });
     }
 };
 
-
-
 const authorisation = async (req, res, next) => {
     try {
-        let userId = req.params.userId
+        let userId = req.params.userId;
         if (userId === ":userId") {
-            return res.status(400).send({ status: false, data: "userId required" })
+            return res.status(400).send({ status: false, data: "userId required" });
         }
 
         if (!ObjectId.isValid(userId)) {
             return res.status(400).send({
                 status: false, data: "please enter valid userID"
-            })
+            });
         }
-        const getUser = await userSchema.findById(userId)
+        const getUser = await userSchema.findById(userId);
         if (!getUser) {
-            return res.status(404).send({ status: false, data: "user not exist" })
-
+            return res.status(404).send({ status: false, data: "user not exist" });
         }
-        if (req.decodeToken.userId !== userId) {
-            return res.status(403).send({ status: false, data: "not authorized" })
+        if (req.decoded.userId !== userId) {
+            return res.status(403).send({ status: false, data: "not authorized" });
         }
 
-        next()
-
+        next();
     } catch (err) {
-        return res.status(500).send({ status: false, data: err.message })
+        return res.status(500).send({ status: false, data: err.message });
     }
-}
+};
 
-module.exports = { authentication, authorisation }
+module.exports = { authentication, authorisation };
