7b49b01dd28b30d7210821092df06c04b84c5b3a
Pawan <pawanrajput852710@gmail.com>
2023-09-09 17:27:12 +0530

    done

--- src/controller/userRegistration.js
+++ src/controller/userRegistration.js
@@ -176,7 +176,7 @@ const updateApplicationStatus = async (req, res) => {
     const applicationId = req.params.applicationId;
     const newStatus = req.body.status;
 
-    const user = await User.findById(userId);
+    const user = await userModel.findById(userId);
 
     if (!user) {
       return res.status(404).json({ success: false, message: "User not found" });
