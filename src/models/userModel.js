51ba4e52b1a9d596c76ff6d91bdca78116eecf19
Pawan <pawanrajput852710@gmail.com>
2023-09-18 22:09:09 +0530

    done

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,95 +1,97 @@
-const mongoose = require("mongoose")
-
-const user = new mongoose.Schema({
-
-
+const userSchema = new Schema({
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        enum: ["admin", "user"],
-
+      type: String,
+      enum: ["admin", "user"],
     },
-    saved: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'App'
-        , status: {
-            type: String,
-            enum: ["I am using it 👍", "Yes, i want to 🤩", "No, i don't 😑", "Maybe 🤔"],
-            default: 'Maybe 🤔'
-
-        }, comment: [{
+    saved: [
+      {
+        obj_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'App',
+        },
+        status: {
+          type: String,
+          enum: ["I am using it 👍", "Yes, i want to 🤩", "No, i don't 😑", "Maybe 🤔"],
+          default: 'Maybe 🤔',
+        },
+        comment: [
+          {
             type: mongoose.Schema.Types.ObjectId,
             ref: "comment",
-            required: true
-        }], user_ratings: [{
+            required: true,
+          },
+        ],
+        user_ratings: [
+          {
             type: mongoose.Schema.Types.ObjectId,
             ref: "rating",
-            required: true
-        }]
-
-    }],
-    following_app: [
-        {
-            obj_id: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'App'
-            },
-            status: {
-                type: String,
-                enum: ["I am using it 👍", "Yes, i want to 🤩", "No, i don't 😑", "Maybe 🤔"],
-                default: 'Maybe 🤔'
-
-            },
-            subscription: {
-                date: {
-                    type: Date,
-                    default: Date.now()
-                }, amount: {
-                    type: Number,
-                    required: true,
-                    default: 0
-                }, duration: {
-                    type: String,
-                    required: true,
-
-                }, package: {
-                    type: String,
-                    default: "trying"
-
-                },
-                comment: [{
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "comment",
-                    required: true
-                }], user_ratings: [{
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "rating",
-                    required: true
-                }]
-
-            }
-        }
+            required: true,
+          },
+        ],
+      },
     ],
-
-    // enter user data ,
-    //  pricing data ,
-
-}, {
+    following_app: [
+      {
+        obj_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'App',
+        },
+        status: {
+          type: String,
+          enum: ["I am using it 👍", "Yes, i want to 🤩", "No, i don't 😑", "Maybe 🤔"],
+          default: 'Maybe 🤔',
+        },
+        subscription: {
+          date: {
+            type: Date,
+            default: Date.now(),
+          },
+          amount: {
+            type: Number,
+            required: true,
+            default: 0,
+          },
+          duration: {
+            type: String,
+            required: true,
+          },
+          package: {
+            type: String,
+            default: "trying",
+          },
+          comment: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "comment",
+              required: true,
+            },
+          ],
+          user_ratings: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "rating",
+              required: true,
+            },
+          ],
+        },
+      },
+    ],
+  }, {
     timestamps: true,
-    get: time => time.toDateString()
-})
-
-
-
-module.exports = mongoose.model("user", user)
+    get: time => time.toDateString(),
+  });
+  
+  module.exports = mongoose.model("User", userSchema);
+  
