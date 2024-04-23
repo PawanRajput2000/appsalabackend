b5e5ae7bd5a174baba40e427ed354b8f10826941
rafi <pawanrajput852710@gmail.com>
2024-04-23 16:46:47 +0530

    done

--- src/controller/category.js
+++ src/controller/category.js
@@ -1,4 +1,5 @@
 const CategorySchema = require("../models/category")
+const App = require("../models/appschema")
 
 const savecategory = async (req, res) => {
     try {
@@ -37,17 +38,49 @@ const fetchSubcategory = async (req, res) => {
 
 
 
+// const fetchCategory = async (req, res) => {
+//     try {
+//         let data = await CategorySchema.find().populate("subCategory_ids");
+
+//         // Fetch all category data
+//         data = data.map((item) => ({
+//             ...item.toObject(),
+//             _id: item._id.toString(), // Convert ObjectId to string for better representation
+//         }));                
+
+//         // Function to filter categories with non-empty subCategory_ids
+//         const filterNonEmptySubcategories = (category) => {
+//             return category.subCategory_ids && category.subCategory_ids.length > 0;
+//         };
+
+//         // Filter categories with non-empty subCategory_ids
+//         const finalData = data.filter(filterNonEmptySubcategories);
+
+
+//         // Respond with the filtered data
+//         return res.json({ status: true, data: finalData });
+//     } catch (err) {
+//         console.log(err.message);
+//         return res.status(500).json({ status: false, message: "Internal Server Error" });
+//     }
+// };
+
 const fetchCategory = async (req, res) => {
     try {
         // Fetch all categories from the CategorySchema
         let categories = await CategorySchema.find().populate("subCategory_ids");
 
-        // Filter categories with non-empty subCategory_ids
+        // Fetch the slugs of existing categories from the App schema
+        let existCategorySlugs = await App.find().distinct("Category");
+        
+        // Filter categories based on whether any sub-category's slug matches with existCategorySlugs
         const filteredCategories = categories.filter(category => {
-            return category.subCategory_ids && category.subCategory_ids.length > 0;
+            return category.subCategory_ids.some(subCategory => existCategorySlugs.includes(subCategory.slug));
         });
 
-        return res.json({ status: true, data: filteredCategories });
+        const finalFiltered = filteredCategories.filter(category => category.subCategory_ids.length > 0);
+
+        return res.json({ status: true, data: finalFiltered });
     } catch (err) {
         console.log(err.message);
         return res.status(500).json({ status: false, message: "Internal Server Error" });
